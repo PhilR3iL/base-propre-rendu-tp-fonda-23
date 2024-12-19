95fb617157e3f2868caec49c9b9f9816cf6de479
Philippe ROUSSILLE <117383554+PhilR3iL@users.noreply.github.com>
2024-12-19 11:47:41 +0100

    Tests de base (à éditer)

---
+++ src/test/java/fr/quackspace/AppTest.java
@@ -0,0 +1,12 @@
+package fr.quackspace;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void testMain() {
+        assertEquals(2, 1 + 1, "1 + 1 devrait être égal à 2");
+    }
+}
